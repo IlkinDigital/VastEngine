7c0ec20cd37bfe35221ae68059fd41c405720481
LesterDigital <lester54539@gmail.com>
2022-05-12 15:46:01 +0400

    Logs are disabled for distribution builds.

--- Vast/Source/Vast/Core/Logging/Logger.h
+++ Vast/Source/Vast/Core/Logging/Logger.h
@@ -20,14 +20,28 @@ namespace Vast {
 
 }
 
-#define VAST_CORE_TRACE(...)	::Vast::Logger::GetCoreLogger()->trace(__VA_ARGS__)
-#define VAST_CORE_INFO(...)		::Vast::Logger::GetCoreLogger()->info(__VA_ARGS__)
-#define VAST_CORE_WARN(...)		::Vast::Logger::GetCoreLogger()->warn(__VA_ARGS__)
-#define VAST_CORE_ERROR(...)	::Vast::Logger::GetCoreLogger()->error(__VA_ARGS__)
-#define VAST_CORE_FATAL(...)	::Vast::Logger::GetCoreLogger()->critical(__VA_ARGS__)
+#ifndef VAST_CONFIG_DISTRIBUTION
+	#define VAST_CORE_TRACE(...)	::Vast::Logger::GetCoreLogger()->trace(__VA_ARGS__)
+	#define VAST_CORE_INFO(...)		::Vast::Logger::GetCoreLogger()->info(__VA_ARGS__)
+	#define VAST_CORE_WARN(...)		::Vast::Logger::GetCoreLogger()->warn(__VA_ARGS__)
+	#define VAST_CORE_ERROR(...)	::Vast::Logger::GetCoreLogger()->error(__VA_ARGS__)
+	#define VAST_CORE_FATAL(...)	::Vast::Logger::GetCoreLogger()->critical(__VA_ARGS__)
 
-#define VAST_TRACE(...)			::Vast::Logger::GetClientLogger()->trace(__VA_ARGS__)
-#define VAST_INFO(...)			::Vast::Logger::GetClientLogger()->info(__VA_ARGS__)
-#define VAST_WARN(...)			::Vast::Logger::GetClientLogger()->warn(__VA_ARGS__)
-#define VAST_ERROR(...)			::Vast::Logger::GetClientLogger()->error(__VA_ARGS__)
-#define VAST_FATAL(...)			::Vast::Logger::GetClientLogger()->critical(__VA_ARGS__)
+	#define VAST_TRACE(...)			::Vast::Logger::GetClientLogger()->trace(__VA_ARGS__)
+	#define VAST_INFO(...)			::Vast::Logger::GetClientLogger()->info(__VA_ARGS__)
+	#define VAST_WARN(...)			::Vast::Logger::GetClientLogger()->warn(__VA_ARGS__)
+	#define VAST_ERROR(...)			::Vast::Logger::GetClientLogger()->error(__VA_ARGS__)
+	#define VAST_FATAL(...)			::Vast::Logger::GetClientLogger()->critical(__VA_ARGS__)
+#else
+	#define VAST_CORE_TRACE(...)
+	#define VAST_CORE_INFO(...)	
+	#define VAST_CORE_WARN(...)	
+	#define VAST_CORE_ERROR(...)
+	#define VAST_CORE_FATAL(...)
+
+	#define VAST_TRACE(...)		
+	#define VAST_INFO(...)		
+	#define VAST_WARN(...)		
+	#define VAST_ERROR(...)		
+	#define VAST_FATAL(...)		
+#endif
